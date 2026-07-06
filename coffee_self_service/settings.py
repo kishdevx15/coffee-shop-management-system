af3aee865af730400147540630d7a7cb24a76f99
kishdevx15 <kishoredevx@gmail.com>
2026-07-06 21:57:42 +0530

    Configure project for Render deployment

--- coffee_self_service/settings.py
+++ coffee_self_service/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+b-j5(=!)ae&r=b_#305l!g+21b#_7ciy_+iq-ou+&fjv9y1cn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
