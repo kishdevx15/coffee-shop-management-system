976476973df5b95b35882e7dd36180a77ea559e9
kishdevx15 <kishoredevx@gmail.com>
2026-08-18 13:04:00 +0530

    Remove cloudinary_storage from INSTALLED_APPS - fixes collectstatic conflict with whitenoise

--- coffee_self_service/settings.py
+++ coffee_self_service/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'core'
